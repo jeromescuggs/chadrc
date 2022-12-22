1e8b14c517864498c419331b6b3a9f824bbc654c
jeromescuggs <subspacer@gmail.com>
2022-12-21 20:03:27 -0600

    git r done

---
+++ plugins/overrides.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "typescript",
+    "c",
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  ignore_buffer_on_setup = true,
+  open_on_setup = true,
+  open_on_setup_file = true,
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
