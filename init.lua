9300752351f673cc1188bda8ad4eca71ca257fdd
jeromescuggs <subspacer@gmail.com>
2022-12-21 20:48:06 -0600

    git r done

--- init.lua
+++ init.lua
@@ -1,4 +1,13 @@
 local autocmd = vim.api.nvim_create_autocmd
+local g = vim.g
+local opt = vim.opt
+    
+
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.cc = 80
+
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
